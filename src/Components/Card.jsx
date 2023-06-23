48e6ac6f11383c94f5fbe8a152379fb73494908d
tadeogavensky <tadeogavensky.email@gmail.com>
2023-06-23 10:48:54 -0300

    cambios

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ContextGlobal } from "./utils/global.context";
 
 const Card = (props) => {
-  const addFav = () => {
-    // Aqui iria la logica para agregar la Card en el localStorage
+  const { state, addFav } = useContext(ContextGlobal);
+
+  const handleFav = () => {
+    addFav(props.dentist);
   };
 
   return (
     <div className="card">
-      <h4>{props.dentist.name}</h4>
-      <h1>{props.dentist.username}</h1>
-
-      <p>{props.dentist.id}</p>
-      <button onClick={addFav} className="favButton">
+      <Link className="col" to={`detail/${props.dentist.id}`}>
+        <h4>{props.dentist.name}</h4>
+        <h1>{props.dentist.username}</h1>
+        <p>{props.dentist.id}</p>
+      </Link>
+      <button onClick={handleFav} className="favButton">
         Add fav
       </button>
     </div>
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import React, { useContext } from "react";
+import { ContextGlobal } from "../Components/utils/global.context";
 
 const Detail = () => {
+  const { state } = useContext(ContextGlobal);
+  const { theme } = state;
  
-  // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
-
   return (
     <>
       <h1>Detail Dentist id </h1>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
     </>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
