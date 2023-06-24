1af389a6e3f5208c785922fbfdf998c9d48167ca
felivander <feli.vander@gmail.com>
2023-06-24 09:38:26 -0300

    feli css

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { ContextGlobal } from "./utils/global.context";
+import doctor from "../assets/images/doctor.jpg"
 
 const Card = (props) => {
   const { state, addFav, removeFav } = useContext(ContextGlobal);
@@ -21,8 +22,9 @@ const Card = (props) => {
 
   return (
     <div className="card">
-      <h4>{props.dentist.name}</h4>
       <h1>{props.dentist.username}</h1>
+      <img src={doctor}/>
+      <h4>{props.dentist.name}</h4>
       <p>{props.dentist.id}</p>
 
       <Link className="col" to={`detail/${props.dentist.id}`}>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ContextGlobal } from "./utils/global.context";
 import { THEME } from "../assets/themes/theme";
+import img from "../assets/images/DH.png";
+
 const Navbar = () => {
   const { state, setTheme } = useContext(ContextGlobal);
   const { theme } = state;
@@ -14,13 +16,21 @@ const Navbar = () => {
 
   return (
     <nav className={theme === THEME.darkMode ? "dark" : ""}>
+      <img src={img}></img>
+
       <div className="row">
         <Link to={"/"}>Home</Link>
         <Link to={"/contact"}>Contact</Link>
         <Link to={"/favs"}>Favs</Link>
       </div>
 
-      <button onClick={toggleTheme}>Change theme</button>
+      <label class="toggle">
+        <span class="icon sun"></span>
+        <span class="icon moon"></span>
+        <input onClick={toggleTheme} type="checkbox"/>
+        <span class="slider"></span>
+      </label>
+
     </nav>
   );
 };
